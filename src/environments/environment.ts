7ad8d6a2933a3e4ef9bd69eb5d9a0ff27cd3e4bb
sarathirangasamy <sarathirangasamy@gmail.com>
2024-12-13 16:03:39 +0530

    url changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
-
 export const environment = {
   production: false,
-  registerStudent: 'http://localhost:3333/api/register-student',
-  apiPort: 'https://intern-server-luhp.onrender.com',
+  apiPort: "https://intern-server-luhp.onrender.com",
   // apiPort: 'http://localhost:3333',
 };
