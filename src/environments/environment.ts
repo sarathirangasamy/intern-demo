0d0fc43afed92d5bc17fa0dbbce173f11f6f5d89
sarathirangasamy <sarathirangasamy@gmail.com>
2024-12-13 15:54:36 +0530

    add port number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@
 export const environment = {
   production: false,
   registerStudent: 'http://localhost:3333/api/register-student',
-  apiPort: 'http://localhost:3333',
+  apiPort: 'https://intern-server-luhp.onrender.com',
+  // apiPort: 'http://localhost:3333',
 };
